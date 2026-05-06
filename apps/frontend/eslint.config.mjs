8f9efb02b7a5a796eec29d31656d1c60b22b591d
olexenn <dudnickalex@gmail.com>
2026-05-06 12:56:24 +0300

    fix(frontend): enable next eslint

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,42 +1,24 @@
 // @ts-check
-import { config } from "typescript-eslint";
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
-import baseConfig from "../../eslint.config.js";
 import pluginNext from "@next/eslint-plugin-next";
+import nextTs from "eslint-config-next/typescript"
 
-export default config(
-  baseConfig,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   {
-    plugins: {
-      "@next/next": pluginNext,
-    },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
     rules: {
-      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules
     },
+    // files: ["**/*.ts", "**/*.tsx"]
   },
-);
+  globalIgnores([
+    '.next/',
+    'out/',
+    'build/',
+    'next.env.d.ts'
+  ])
+])
 
-// const eslintConfig = defineConfig([
-//   ...nextVitals,
-//   {
-//     rules: {
-//       ...pluginNext.configs.recommended.rules
-//     },
-//     plugins: {
-//       "@next/next": pluginNext
-//     },
-//     files: ["**/*.ts", "**/*.tsx"]
-//   },
-//   globalIgnores([
-//     '.next/',
-//     'out/',
-//     'build/',
-//     'next.env.d.ts'
-//   ])
-// ])
-
-// export default eslintConfig
+export default eslintConfig
